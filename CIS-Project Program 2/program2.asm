ca647adbc0e93917b6832f6d4c40f2ac189be63d
Jondell Samson <jsams001@ucr.edu>
2018-05-22 16:41:57 -0700

    Revert "start of final commits"
    This reverts commit 21997c9960cdb8cf99fd9a7fb2008612cd6a12c7.

--- CIS-Project Program 2/program2.asm
+++ CIS-Project Program 2/program2.asm
@@ -1,10 +1,3 @@
-; CIS-11 Final Project - Program 2
-; Group: Ctrl See - Jondell Samson, Marcos Padilla, Cesar 
-; Purpose: User types in name and program reverses it in console. 
-; User types ok in message box to terminate program
-; Note: Must have masm32 and Project folders all installed
-; and running on the same drive, preferably C:\
-
 .486                                    ; create 32 bit code
 .model flat, stdcall                    ; 32 bit memory model
 option casemap :none                    ; case sensitive
@@ -20,15 +13,11 @@ includelib \masm32\lib\gdi32.lib
 includelib \masm32\lib\user32.lib
 includelib \masm32\lib\kernel32.lib
 
-
 .data
-Message db "Your name reversed is in the console window.", 0
+Mess db "Your string reversed is in the console window.", 0
 Top db "Completed", 0
 original_text PROTO :DWORD
-
-.data?
-originalstr DWORD 60 DUP(?)
-reversestr DWORD 60 DUP(?)
+reverse_text PROTO :DWORD
 
 .code                       
 start:                          
@@ -36,11 +25,12 @@ start:
     exit
 
 main proc
-        
-    mov originalstr, input("Type your name: ")
-    invoke original_text, originalstr
-	call reverse_text								; procedure used to reverse the string
-	invoke MessageBox,NULL,Addr Message,Addr Top,MB_OK
+
+    LOCAL txtinput:DWORD        
+    mov txtinput, input("Type your name: ")
+    invoke original_text, txtinput
+	invoke reverse_text, txtinput ; procedure used to reverse the string
+	invoke MessageBox,NULL,Addr Mess,Addr Top,MB_OK
     ret
 main endp
 
@@ -52,11 +42,27 @@ original_text proc string:DWORD
     ret
 original_text endp 
 
-reverse_text proc
+reverse_text proc uses eax ebx ecx esi string:DWORD
 
-	print chr$("This is your name backwards:",13,10)
-	invoke szRev, originalstr, addr reversestr
-	print offset reversestr, 13, 10
+	LOCAL reversestr:DWORD
+
+	lea ebx, reversestr					; ebx traverses through the reversestring label
+	mov ecx, SIZEOF string				; ecx would be used to get the last value of the string, and 
+										; would decrement until you hit the first value of the string
+	lea eax, string[ecx]				; eax points to the first value in the string
+
+	reverse:
+		mov esi, [eax]
+		mov [ebx], esi
+		inc ebx
+		dec eax
+		loop reverse
+		jmp PrintRev 
+
+	PrintRev:
+		print chr$("This is your name backwards:",13,10)
+		print reversestr                ; show the string at the console
+		print chr$(13,10)
     ret
 reverse_text endp 
 
